31e35e9bf670423f2ae8c2f6c194537a7a40ff7f
nogifeet <72322393+nogifeet@users.noreply.github.com>
2020-11-24 19:52:23 +0530

    Update traffic-singal_using_led.py

--- Traffic Signal Led/traffic-singal_using_led.py
+++ Traffic Signal Led/traffic-singal_using_led.py
@@ -1,5 +1,5 @@
 #led example for traffic Signal
-#each signal will be lit for 10 seconds then turn off
+#each signal will be lit for 10 seconds then turn off(except Yellow)
 #the signal will run for 5 interations
 #order Red-10 seconds, Yellow - 5 Seconds, Green - 10 Seconds similar to Real Life Traffic Signal
 
@@ -24,7 +24,7 @@ for i in range(3):
     time.sleep(10)
     GPIO.output(23, GPIO.LOW)
     
-    #turn on the yellow signal for 10 sec
+    #turn on the yellow signal for 5 sec
     GPIO.output(17, GPIO.HIGH)
     time.sleep(5)
     GPIO.output(17, GPIO.LOW)
