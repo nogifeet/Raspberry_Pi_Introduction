b1c43031b27535a242cd143c9efa715331cc99ba
nogifeet <72322393+nogifeet@users.noreply.github.com>
2020-11-27 15:49:56 +0530

    Update camera_live_streaming.py

--- camera_live_streaming.py
+++ camera_live_streaming.py
@@ -1,7 +1,4 @@
 # Web streaming example
-# Source code from the official PiCamera package
-# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
-
 import io
 import picamera
 import logging
